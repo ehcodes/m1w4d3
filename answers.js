deefda8bd85d56ff9fbc30939a830dba75c0d4e3
ehcodes <engrid.hernandez@gmail.com>
2023-07-27 11:16:32 -0400

    Where is Waldo answered

--- answers.js
+++ answers.js
@@ -108,10 +108,17 @@ console.log(favMovies.slice(0,midIndex)) */
 // favMovies with const, and yet, we were allowed to
 // change the array. Weird? Should we have used let?
 //-----------------------------
-// subheading
+// Where is Waldo
 //-----------------------------
-// console.log(xxx)
-
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert", ["Lucinda", "Jacc", "Neff", "Snoop"], ["Petunia", ["Baked Goods", "Waldo"]]];
+// Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1,1)
+console.log(whereIsWaldo)
+// Change "Neff" to "No One"
+whereIsWaldo[1].splice(2,1,"No One")
+console.log(whereIsWaldo)
+// Access and console.log "Waldo"
+console.log(whereIsWaldo[2][1][1])
 //-----------------------------
 // subheading
 //-----------------------------
